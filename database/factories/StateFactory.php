8d435ad8993b694b78391404ae8b36a1ecff9c28
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-11-10 18:07:37 +0600

    add factory seeder codes

--- database/factories/StateFactory.php
+++ database/factories/StateFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class StateFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->state(),
+            'country_id' => Country::factory(),
         ];
     }
 }
